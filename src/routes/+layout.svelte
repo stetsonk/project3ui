101e9daaf92cae8c2d65708865070c073a27444f
Stetson King <Stetson.e.king@outlook.com>
2024-11-15 14:13:01 -0500

    added header component and some logos

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
-	import '../app.css';
-	let { children } = $props();
+    import '../app.css';
+    import Header from '../components/Header.svelte';
+    let { children } = $props();
 </script>
 
+<Header />
 {@render children()}
